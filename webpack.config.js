2e0bf1647de03f4645e6e79288b4d21ed2c5591a
ali <alihcompiler@gmail.com>
2019-02-22 23:29:18 +0300

    rename index.ts to example.ts

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/example.ts',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
